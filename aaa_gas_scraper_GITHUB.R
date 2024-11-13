d5f1864d5b860182694fa1f4c009872ece3f4b83
Michael Davidson <mdavidson@appliedanalysis.com>
2024-11-13 13:02:32 -0800

    Update aaa_gas_scraper_GITHUB.R
    Updated scraper to pivot data to a long format. Fixed the Reno and Las Vegas data labeling.

--- aaa_gas_scraper_GITHUB.R
+++ aaa_gas_scraper_GITHUB.R
@@ -15,12 +15,23 @@ gas_df <- gas_df %>%
   mutate(key = rep(c('Price', 'Regular', 'Mid', 'Premium', 'Diesel'), n() / 5), 
          id = cumsum(key == 'Price')) %>% 
   spread(key, gas_nv) %>% 
-  select(id, Regular, Mid, Premium, Diesel) %>% 
-  mutate(Date = Sys.Date())
+  select(id, Regular, Mid, Premium, Diesel) 
 
 gas_df$id[which(gas_df$id == "1")] <- "Nevada"
-gas_df$id[which(gas_df$id == "2")] <- "Las_Vegas"
-gas_df$id[which(gas_df$id == "3")] <- "Reno"
+gas_df$id[which(gas_df$id == "2")] <- "Reno"
+gas_df$id[which(gas_df$id == "3")] <- "Las Vegas"
+
+
+#Pivot data long
+gas_df <- gas_df %>%
+  pivot_longer(
+    cols = c(2,3,4,5), # Select columns to pivot longer by index
+    names_to = "Gas_Type",
+    values_to = "Price")
+
+#Add a date columm
+gas_df <- gas_df %>% 
+  mutate(Scrape_Date = Sys.Date())
 
 ##
 #Make a backup of the full scrape
